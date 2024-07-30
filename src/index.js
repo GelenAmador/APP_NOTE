5c90bfc0feb48264c6d4c75a65d6cb3eeea41a8d
GelenAmador <98992768+GelenAmador@users.noreply.github.com>
2024-07-30 11:51:15 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Cambiado para usar createRoot
+import ReactDOM from 'react-dom/client'; 
 import App from './App';
 import './index.css';
 
